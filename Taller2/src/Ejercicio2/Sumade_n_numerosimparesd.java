5c436909c6dfe7cfc2cc0f59ab0d98df5d432ed8
Diego Huamanjulca <diegohuamnjulcaguerrero2002@gmail.com>
2020-12-15 01:14:19 -0500

    Agregue el ejercicio2

---
+++ Taller2/src/Ejercicio2/Sumade_n_numerosimparesd.java
@@ -0,0 +1,24 @@
+package Ejercicio2;
+import javax.swing.JOptionPane;
+
+public class Sumade_n_numerosimparesd {
+    public static void main(String[] args) {
+        int i, n, sumaimp;
+        JOptionPane.showMessageDialog(null,"Sumemos numero impares", "Bienvenido", 1 );
+        n = Integer.parseInt(JOptionPane.showInputDialog (null, "¿Hasta que numero quiere que llegue la suma?","Inserte numero" , 3));
+	sumaimp = 0;
+	while (n<0) {
+                JOptionPane.showMessageDialog(null,"No puede ser numero negativo", "Positivo", 2 );
+                JOptionPane.showMessageDialog(null,"Por favor ingrese un numero positivo", "Positivo", 1 );
+		n = Integer.parseInt(JOptionPane.showInputDialog (null, "¿Hasta que numero quiere que llegue la suma?","Inserte numero" , 3));
+	}
+	for (i=1;i<=n;i++) {
+		if (i%2>0) {
+			sumaimp = sumaimp+i;
+		}
+	}
+        
+	String mensaje = "La suma es: "+sumaimp;
+                 JOptionPane.showMessageDialog(null, mensaje, "Resultado", 1);
+    }
+}
