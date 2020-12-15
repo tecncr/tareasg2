395ebfd595c0a0a39b28aef705e3279728568137
Diego Huamanjulca <diegohuamnjulcaguerrero2002@gmail.com>
2020-12-15 01:02:56 -0500

    Agregue el ejercicio4

---
+++ Taller2/src/Ejercicio4/Factorialdenumero.java
@@ -0,0 +1,29 @@
+package Ejercicio4;
+import javax.swing.JOptionPane;
+
+public class Factorialdenumero {
+    public static void main(String[] args) {
+        int cont, factorial, numero;
+        JOptionPane.showMessageDialog(null,"Calculemos el factorial de un numero", "Bienvenido", 1 );
+        numero = Integer.parseInt(JOptionPane.showInputDialog (null, "Ingrese un numero","Inserte numero" , 3));
+	while (numero<0) {
+		JOptionPane.showMessageDialog(null,"El numero es negativo, no existe facrotial de numeros negativos", "Positivo", 2 );
+                JOptionPane.showMessageDialog(null,"Por favor ingrese un numero positivo", "Positivo", 1 );
+		  numero = Integer.parseInt(JOptionPane.showInputDialog (null, "Ingrese un numero","Inserte numero" , 3));
+	}
+	if (numero>=0) {
+		cont = 0;
+		factorial = 1;
+		while (cont<numero) {
+			cont = cont+1;
+			factorial = factorial*cont;
+		}
+                  String mensaje = "El factorial es: "+factorial;
+                 JOptionPane.showMessageDialog(null, mensaje, "Resultado", 1);
+	} else {
+		if (numero==0 || numero==1) {
+			JOptionPane.showMessageDialog(null,"El factorial del numero es: 1", "Resultado", 1 );
+		}
+	}
+    }
+}
