04145b2ac8875244e4d1ca2031faf0a6b72186bc
Diego Huamanjulca <diegohuamnjulcaguerrero2002@gmail.com>
2020-12-15 02:11:26 -0500

    Agregue el ejercicio7

---
+++ Taller2/src/Ejercicio7/Simplifiarfraccion.java
@@ -0,0 +1,26 @@
+package Ejercicio7;
+import javax.swing.JOptionPane;
+
+public class Simplifiarfraccion {
+    public static void main(String[] args) {
+       int a, b, denominador, n1, n2, numerador;
+	JOptionPane.showMessageDialog(null," Simplifiquemos una fraccion", "Bienvenido", 1 );
+        a = Integer.parseInt(JOptionPane.showInputDialog (null, "Ingrese el numerador","Inserte numero" , 3));
+        b = Integer.parseInt(JOptionPane.showInputDialog (null, "Ingrese el denominador","Inserte numero" , 3));
+	n1 = a;
+	n2 = b;
+        while (n1!=n2) {
+		if (n1>n2) {
+			n1 = n1-n2;
+		} else {
+			n2 = n2-n1;
+		}
+	}
+	numerador = a/n1;
+	denominador = b/n1;
+                String mensaje = "Al simpliflicar la fraccion: ";
+                mensaje += (a + "/" + b);
+                mensaje += ("\nQueda: " + numerador + "/" + denominador);
+                JOptionPane.showMessageDialog(null, mensaje,  "Resultado", 1);
+    }
+}
